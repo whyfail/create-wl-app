849034e9bc03470e6314f11e6a7468140e56b73c
吴磊 <wulei@santu.com>
2022-09-29 15:53:29 +0800

    [初始化项目]

---
+++ README.md
@@ -0,0 +1,24 @@
+# create-wl-app 使用指南
+
+## 1. 脚手架介绍
+
+开发者使用 create-wl-app 拉取项目模板，快速完成项目初始化
+
+## 2. 全局安装
+
+```js
+npm i create-wl-app -g
+```
+
+## 3. 命令行
+
+- create-wl-app -V ：查看 create-wl-app 版本
+- create-wl-app list ：查看模板列表
+- create-wl-app create ：选择一个模板生成项目
+- create-wl-app add ：添加一个模板
+- create-wl-app delete ：删除一个模板
+
+## 4. 内置模板列表
+
+- vite-react
+- vite-vue3
---
+++ bin/create-wl-app.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+const program = require("commander")
+const config = require("../package.json")
+
+program.version(config.version, "-v, --version")
+
+// 定义使用方法
+program
+  .command("create")
+  .description("选择模板生成项目")
+  .action(() => {
+    require("../lib/create")
+  })
+
+program
+  .command("list")
+  .description("查看当前可用模板")
+  .action(() => {
+    require("../lib/list")
+  })
+
+program
+  .command("add")
+  .description("添加模板")
+  .action(() => {
+    require("../lib/add")
+  })
+
+program
+  .command("delete")
+  .description("删除模板")
+  .action(() => {
+    require("../lib/delete")
+  })
+
+// 解析参数
+program.parse(process.argv)
+if (!program.args.length) {
+  program.help()
+}
